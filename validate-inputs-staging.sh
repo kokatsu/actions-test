5b3285b63a2cb22e8fba780e564e5e501dd9504d
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-05-08 12:30:02 +0900

    feat: fix validate-inputs-staging.sh

--- validate-inputs-staging.sh
+++ validate-inputs-staging.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-# 入力値の確認
 keyword="$1"
 
-# キーワードが banana の場合に失敗させる
 if [[ "$keyword" == "dekopon" ]]; then
-  echo "Error: Keyword cannot be banana for staging environment"
+  echo "Error: Keyword cannot be dekopon for staging environment"
   exit 1
 fi
 
-# 条件を満たしている場合は成功を出力
 echo "Inputs validation successful: Keyword is $keyword"
 exit 0
